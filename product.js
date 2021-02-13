3185eb1880e9cb7ccee4bcc608943569875db60a
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-02-13 10:19:56 +0100

    last touches on product site - dynamic

--- product.js
+++ product.js
@@ -1,5 +1,5 @@
 //shorten the link
-const url = "https://kea-alt-del.dk/t7/api/products/1164";
+const url = "https://kea-alt-del.dk/t7/api/products/1543";
 //fetch the data
 fetch(url)
   .then((res) => res.json())
@@ -10,14 +10,20 @@ fetch(url)
 function showProduct(product) {
   console.log(product);
 
-  document.querySelector(".product_img").src =
+  document.querySelector(".category").textContent = product.subcategory + "/ ";
+  document.querySelector(".product_name").textContent =
+    product.productdisplayname;
+
+  document.querySelector(".product_image").src =
     "https://kea-alt-del.dk/t7/images/webp/640/" + product.id + ".webp";
+  document.querySelector(".product_image").alt = product.productdisplayname;
   document.querySelector(".brand").textContent = product.brandname;
   document.querySelector("h1").textContent = product.productdisplayname;
-  document.querySelector(".description"). = product.description;
+  document.querySelector(".price").textContent = product.price;
+  document.querySelector(".description").innerHTML = product.description;
   document.querySelector(".color_box").style.backgroundColor =
     product.basecolour;
 }
 
-// <li class="price"><p>895kr</p></li>
-
+// <li class="category"><a href="product_list.css">Topwear/ </a></li>
+//<li class="product_name">Sahara Team India Fanwear Round Neck Jersey<a href=""></a></li>
